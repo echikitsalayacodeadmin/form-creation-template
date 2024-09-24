b33051e1168391017a690f82793e265ffbc6b22e
ayush-uno <dahariaayush@uno.care>
2024-09-25 02:07:24 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import UltraTech from "./ultratech/ultraTech";
-import GenericFormGenerator from "./genericFormGenerator/genericFormGenerator";
-import UltratechDepartmentWise from "./ultratech/ultratechDepartmentWise";
-import VacinationCertificate from "./adaniWilmar/vacinationCertificate";
-import VacinationVIkram from "./vikram/vacinationVIkram";
-import BloodPdf from "./blood/bloodPdf";
-import UshaKiranPalace from "./ushaKiranPalace/ushaKiranPalace";
-import Hershey from "./hershey/hershey";
-import Pepsico from "./pepsico/pepsico";
+// import UltraTech from "./ultratech/ultraTech";
+// import GenericFormGenerator from "./genericFormGenerator/genericFormGenerator";
+// import UltratechDepartmentWise from "./ultratech/ultratechDepartmentWise";
+// import VacinationCertificate from "./adaniWilmar/vacinationCertificate";
+// import VacinationVIkram from "./vikram/vacinationVIkram";
+// import BloodPdf from "./blood/bloodPdf";
+// import UshaKiranPalace from "./ushaKiranPalace/ushaKiranPalace";
+// import Hershey from "./hershey/hershey";
+// import Pepsico from "./pepsico/pepsico";
+import UnichemAnnexure from "./unichem/unichemAnnexure";
 
 const App = () => {
   return (
     <div>
       {/* <VacinationCertificate /> */}
-      <Pepsico />
+      <UnichemAnnexure />
       {/* <GenericFormGenerator /> */}
     </div>
   );
